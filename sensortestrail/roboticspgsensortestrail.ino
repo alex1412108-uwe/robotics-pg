c7b96a41ef4a774a868063b3f7338c8009115428
alex141108 <alex1412108@gmail.com>
2015-03-04 10:36:27 +0000

    fix enable pin issue

--- sensortestrail/roboticspgsensortestrail.ino
+++ sensortestrail/roboticspgsensortestrail.ino
@@ -15,7 +15,7 @@ void setup() {
     // set all the other pins you're using as outputs:
     pinMode(motor1Pin1, OUTPUT);
     pinMode(motor1Pin2, OUTPUT);
-    pinMode(enablePin, OUTPUT);
+    //pinMode(enablePin, OUTPUT);
 
     // set enablePin high so that motor can turn on:
     digitalWrite(enablePin, HIGH);
@@ -24,23 +24,27 @@ void setup() {
 }
 
 void loop() {
-    // if the switch is high, motor will turn on one direction:
-    //if (digitalRead(switchPin) == HIGH) {
-        //digitalWrite(motor1Pin1, LOW); // set pin 2 on L293D low
-        //digitalWrite(motor1Pin2, HIGH); // set pin 7 on L293D high
-    //}
-    // if the switch is low, motor will turn in the opposite direction:
-    //else {
-        digitalWrite(motor1Pin1, HIGH); // set pin 2 on L293D high
-        digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D low
-    //}
-  // read the analog in value:
-  
   sensorValue = analogRead(analogInPin);            
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);  
-  // change the analog out value:
-  analogWrite(enablePin, outputValue);           
+    // if the switch is high, motor will turn on one direction:
+  if (sensorValue<500) {
+      digitalWrite(motor1Pin1, LOW); // set pin 2 on L293D low
+      digitalWrite(motor1Pin2, HIGH); // set pin 7 on L293D high
+      // map it to the range of the analog out:
+      outputValue = map(sensorValue, 0, 500, 255, 0);  
+      // change the analog out value:
+      analogWrite(enablePin, outputValue);           
+
+    }
+    // if the switch is low, motor will turn in the opposite direction:
+  else {
+      digitalWrite(motor1Pin1, HIGH); // set pin 2 on L293D high
+      digitalWrite(motor1Pin2, LOW); // set pin 7 on L293D low
+      // map it to the range of the analog out:
+      outputValue = map(sensorValue, 500, 1023, 0, 255);  
+      // change the analog out value:
+      analogWrite(enablePin, outputValue);           
+
+    }          
 
   // print the results to the serial monitor:
   Serial.print("sensor = " );                       
