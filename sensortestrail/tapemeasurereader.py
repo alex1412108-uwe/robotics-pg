dfd9ab5e59500e39d6aec4269af7a7020b468f1e
{{git.name}} <alex1412108@gmail.com>
2015-03-23 02:44:28 +0000

    rotates image, ocr command commented out but ready for integration

--- sensortestrail/tapemeasurereader.py
+++ sensortestrail/tapemeasurereader.py
@@ -1,1 +1,30 @@
 #use tesseract for extracting ocr
+import time
+import argparse
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+from matplotlib import pyplot as plt
+import Image
+import pytesseract
+
+
+folder_input = '/constant_increasing_0deg/data'
+#folder_output = '../results/thresholddetect'
+
+filename = 'test0.bmp'
+
+im = cv2.imread('ocrtest.jpeg')
+rows,cols,temp = im.shape
+
+M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+dst = cv2.warpAffine(im,M,(cols,rows))
+
+cv2.imshow('rotate',dst)
+
+cv2.waitKey(0)
+
+#print pytesseract.image_to_string(Image.open('TestPilotCollective-OCRK-2011-12-05.gif'))
+
+
